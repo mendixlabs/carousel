8f7b9df9f3c1a8d7e572a5d508b8dd4e894771ec
Akileng Isaac <isaacakileng56@gmail.com>
2016-10-06 11:00:30 +0300

    New line at end of file

--- src/ImageCarouselReact/widget/ImageCarouselReactNoContext.js
+++ src/ImageCarouselReact/widget/ImageCarouselReactNoContext.js
@@ -1,5 +1,4 @@
 dojo.require("ImageCarouselReact.widget.ImageCarouselReact");
-// why does require "ImageCarouselReact/widget/ImageCarouselReact" not working?
 define(["dojo/_base/declare"], function (declare) {
     "use strict";
     return declare("ImageCarouselReact.widget.ImageCarouselReactNoContext", [ImageCarouselReact.widget.ImageCarouselReact], {
