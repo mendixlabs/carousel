758d7d59cb0ada589fb07bd0a251555b1770b723
Edwin P. Magezi <emagezi@gmail.com>
2016-10-20 18:17:26 +0300

    Add interval prop to existing tests

--- src/com/mendix/widget/carousel/components/__tests__/Carousel.spec.tsx
+++ src/com/mendix/widget/carousel/components/__tests__/Carousel.spec.tsx
@@ -11,7 +11,7 @@ describe("Carousel component", () => {
     const render = (props: CarouselProps) => shallow(<Carousel {...props} />);
 
     it("should render carousel structure", () => {
-        const output = render({ images: staticImages });
+        const output = render({ images: staticImages, interval: 3000 });
         expect(output).toMatchStructure(
             <div className="carousel">
                 <div className="carousel-inner" />
@@ -20,28 +20,28 @@ describe("Carousel component", () => {
     });
 
     it("should render carousel with 2 children", () => {
-        const wrapper = render({ images: staticImages });
+        const wrapper = render({ images: staticImages, interval: 3000 });
         expect(wrapper.find(".carousel-inner").children().length).toEqual(staticImages.length);
     });
 
     it("should render carousel with the class 'carousel'", () => {
-        const output = render({ images: staticImages });
+        const output = render({ images: staticImages, interval: 3000 });
         expect(output).toHaveClass("carousel");
     });
 
     it("should render carousel child to have class 'carousel-inner'", () => {
-        const output = render({ images: staticImages });
+        const output = render({ images: staticImages, interval: 3000 });
         expect(output.children()).toHaveClass("carousel-inner");
     });
 
     it("first child should be active", () => {
-        const wrapper = render({ images: staticImages });
+        const wrapper = render({ images: staticImages, interval: 3000 });
         const children = wrapper.find(".carousel-inner").children();
         expect(children.first().prop("active")).toBe(true);
     });
 
     it("should have only one active child", () => {
-        const wrapper = render({ images: staticImages });
+        const wrapper = render({ images: staticImages, interval: 3000 });
         const children = wrapper.find(".carousel-inner").children();
         const activeChildren = children.filterWhere(c => c.prop("active") === true);
         expect(activeChildren.length).toBe(1);
