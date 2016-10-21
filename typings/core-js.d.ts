37a617ac6005ce823f6c8dd342c56ffc6cbd684f
Andries Smit <mr.andries.smit@gmail.com>
2016-10-21 09:49:24 +0200

    Fix indents

--- typings/core-js.d.ts
+++ typings/core-js.d.ts
@@ -1,32 +1,6 @@
-// Note for the reviewer: this a subset for core-js
-// The standard npm @types/core-js version does not work for structureMatcher.ts.
-
 // Type definitions for core-js v0.9.7
-// Project: https://github.com/zloirock/core-js/
-// Definitions by: Ron Buckton <http://github.com/rbuckton>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-/* *****************************************************************************
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
-
-THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-MERCHANTABLITY OR NON-INFRINGEMENT.
-
-See the Apache Version 2.0 License for specific language governing permissions
-and limitations under the License.
-***************************************************************************** */
-
 declare type PropertyKey = string | number | symbol;
-
-// #############################################################################################
-// ECMAScript 6: Symbols
-// Modules: es6.symbol
-// #############################################################################################
+declare var Symbol: SymbolConstructor;
 
 interface Symbol {
     toString(): string;
@@ -51,9 +25,6 @@ interface SymbolConstructor {
     useSimple(): void;
     userSetter(): void;
 }
-
-declare var Symbol: SymbolConstructor;
-
 interface Array<T> {
     [Symbol.iterator](): IterableIterator<T>;
 }
