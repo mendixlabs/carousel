9f34ae4c3caba32dda3ad2cfbdd610463a83101b
Edwin P. Magezi <emagezi@gmail.com>
2016-10-21 16:47:24 +0300

    Add tests for Carousel components

--- src/com/mendix/widget/carousel/components/__tests__/CarouselItem.spec.tsx
+++ src/com/mendix/widget/carousel/components/__tests__/CarouselItem.spec.tsx
@@ -1,27 +1,37 @@
-import { shallow } from "enzyme";
+import { ReactWrapper, ShallowWrapper, mount, shallow } from "enzyme";
 import * as React from "react";
 
 import { CarouselItem, CarouselItemProps } from "../CarouselItem";
 
 describe("CarouselItem component", () => {
     const imageUrl = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
-    const render = (props: CarouselItemProps) => shallow(React.createElement(CarouselItem, props));
+    let shallowWrapper: ShallowWrapper<CarouselItemProps, {}>;
 
-    it("should render CarouselItem active", () => {
-        const output = render({ imageUrl, active: true });
-        expect(output).toBeElement(
-            <div className="item active">
-                <img src={ imageUrl } alt="item"/>
-            </div>
-        );
+    beforeEach(() => {
+        shallowWrapper = shallow(<CarouselItem imageUrl={imageUrl} active={true} />);
     });
 
-    it("should render CarouselItem inactive", () => {
-        const output = render({ imageUrl, active: false });
-        expect(output).toBeElement(
-            <div className="item">
+    it("should be a div with classes item & active, with an img element as the only child", () => {
+        expect(shallowWrapper.equals(
+            <div className="item active">
                 <img src={ imageUrl } alt="item" />
             </div>
-        );
+        )).toBe(true);
+    });
+
+    describe("FullDOM", () => {
+        let fullDOMWrapper: ReactWrapper<CarouselItemProps, {}>;
+
+        beforeEach(() => {
+            fullDOMWrapper = mount(<CarouselItem imageUrl={imageUrl} active={true} />);
+        });
+
+        it("should let props be set", () => {
+            expect(fullDOMWrapper.props().imageUrl).toBe(imageUrl);
+            expect(fullDOMWrapper.props().active).toBe(true);
+            fullDOMWrapper.setProps({ active: false, imageUrl: "test.jpg" });
+            expect(fullDOMWrapper.props().imageUrl).toBe("test.jpg");
+            expect(fullDOMWrapper.props().active).toBe(false);
+        });
     });
 });
