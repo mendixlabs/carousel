680fdceab5efd7c8e995d8355cb4913a1bfd526a
Edwin P. Magezi <emagezi@gmail.com>
2016-10-24 16:26:46 +0300

    Review: remove return statement for stateless function

--- src/com/mendix/widget/carousel/components/CarouselItem.ts
+++ src/com/mendix/widget/carousel/components/CarouselItem.ts
@@ -5,10 +5,8 @@ export interface CarouselItemProps {
     active?: boolean;
 }
 
-export const CarouselItem = (props: CarouselItemProps) => {
-    return (
-        DOM.div({ className: props.active ? "item active" : "item" },
-            DOM.img({ alt: "item", src: props.imageUrl })
-        )
-    );
-};
+export const CarouselItem = (props: CarouselItemProps) => (
+    DOM.div({ className: props.active ? "item active" : "item" },
+        DOM.img({ alt: "item", src: props.imageUrl })
+    )
+);
