d2e535f15a36592acbf49da5dda1914189f5c3b1
Edwin P. Magezi <emagezi@gmail.com>
2016-10-24 17:40:18 +0300

    Review: remove unnecessary return keyword

--- src/com/mendix/widget/carousel/components/Carousel.ts
+++ src/com/mendix/widget/carousel/components/Carousel.ts
@@ -9,18 +9,18 @@ export interface CarouselProps {
     images?: Image[];
 }
 
-const carouselItems = (images: Image[]) => {
-    return images.map((image, index) => createElement(CarouselItem, {
+const carouselItems = (images: Image[]) => (
+    images.map((image, index) => createElement(CarouselItem, {
         active: index === 0,
         key: index,
         url: image.url
-    }));
-};
+    })
+));
 
-export const Carousel = (props: CarouselProps) => {
-    return (DOM.div({ className: "carousel" },
+export const Carousel = (props: CarouselProps) => (
+    DOM.div({ className: "carousel" },
         DOM.div({  className: "carousel-inner" },
             carouselItems(props.images)
         )
-    ));
-};
+    )
+);
