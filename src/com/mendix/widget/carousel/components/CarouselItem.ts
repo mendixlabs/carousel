e8767fdf191dbee81e8058407ce66b15b9218951
Edwin P. Magezi <emagezi@gmail.com>
2016-10-24 17:59:38 +0300

    Review: add more descriptive alt

--- src/com/mendix/widget/carousel/components/CarouselItem.ts
+++ src/com/mendix/widget/carousel/components/CarouselItem.ts
@@ -7,6 +7,6 @@ export interface CarouselItemProps {
 
 export const CarouselItem = (props: CarouselItemProps) => (
     DOM.div({ className: "item" + (props.active ? " active" : "") },
-        DOM.img({ alt: "item", src: props.url })
+        DOM.img({ alt: "Carousel image", src: props.url })
     )
 );
