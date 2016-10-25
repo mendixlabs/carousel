582a9c6b0da262ce8a16d37881bfafc74e5a4a3b
Edwin P. Magezi <emagezi@gmail.com>
2016-10-25 18:08:50 +0300

    Review: add missing tests

--- src/com/mendix/widget/carousel/components/__tests__/Carousel.spec.ts
+++ src/com/mendix/widget/carousel/components/__tests__/Carousel.spec.ts
@@ -1,33 +1,95 @@
-import { ShallowWrapper, shallow } from "enzyme";
+import { ReactWrapper, ShallowWrapper, mount, shallow } from "enzyme";
 import { DOM, createElement } from "react";
 
 import { Carousel, CarouselProps, Image } from "../Carousel";
+import { CarouselItem } from "../CarouselItem";
 
 describe("Carousel", () => {
     const images: Image[] = [
         { url: "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" },
         { url: "https://www.google.com/images/nav_logo242.png" }
     ];
-    let wrapper: ShallowWrapper<CarouselProps, any>;
-    beforeEach(() => wrapper = shallow(createElement(Carousel, { images })));
+    let output: ShallowWrapper<CarouselProps, any>;
+    let carouselWrapper: ShallowWrapper<CarouselProps, any>;
 
-    it("should be a 'div' of class carousel with a 'div' child of class carousel-inner", () => {
-        expect(wrapper).toMatchStructure(DOM.div({ className: "carousel" },
-            DOM.div({ className: "carousel-inner" }))
-        );
+    beforeEach(() => {
+        output = shallow(createElement(Carousel, { images }));
+        carouselWrapper = output.find("div.carousel-inner");
     });
 
-    it("should have the number of children equal to the number of images", () => {
-        expect(wrapper.find(".carousel-inner").children().length).toEqual(images.length);
+    it("renders a div of class carousel with a div child of class carousel-inner", () => {
+        const structure = DOM.div({ className: "carousel" },
+            DOM.div({ className: "carousel-inner" }));
+
+        expect(output).toMatchStructure(structure);
     });
 
-    it("should have the first image as active", () => {
-        expect(wrapper.find(".carousel-inner").children().first().prop("active")).toBe(true);
+    it("renders a wrapper with the same number of children as the images", () => {
+        let children = carouselWrapper.children();
+
+        expect(children.length).toEqual(images.length);
     });
 
-    it("should have only one active image", () => {
-        const children = wrapper.find(".carousel-inner").children();
+    it("renders a wrapper with one child", () => {
+        output = shallow(createElement(Carousel, {
+                images: [ { url: "https://www.google.com/images/nav_logo242.png" } ]
+            }
+        ));
 
-        expect(children.filterWhere(c => c.prop("active") === true).length).toBe(1);
+        const children = output.find("div.carousel-inner").children();
+
+        expect(children.length).toEqual(1);
+    });
+
+    it("renders a wrapper with no children", () => {
+        output = shallow(createElement(Carousel));
+
+        const children = output.find("div.carousel-inner").children();
+
+        expect(children.length).toBe(0);
+    });
+
+    it("renders a wrapper with the first child active", () => {
+        const firstChild = carouselWrapper.children().first();
+
+        expect(firstChild.prop("active")).toBe(true);
+    });
+
+    it("renders a wrapper with only one active child", () => {
+        const activeChildren = carouselWrapper.children().filterWhere(c => c.prop("active"));
+
+        expect(activeChildren.length).toBe(1);
+    });
+
+    describe("full component", () => {
+        let fullOutput: ReactWrapper<CarouselProps, any>;
+        let fullWrapper: ReactWrapper<CarouselProps, any>;
+
+        beforeEach(() => {
+            fullOutput = mount(createElement(Carousel, { images }));
+            fullWrapper = fullOutput.find("div.carousel-inner");
+        });
+
+        it("renders a wrapper with children of type CarouselItem", () => {
+            const children = fullWrapper.children();
+
+            children.forEach((child: any) => {
+
+                expect(child.type()).toEqual(CarouselItem);
+            });
+        });
+
+        it("renders CarouselItems properly", () => {
+            const renderedImages = fullOutput.find("div.carousel-inner img");
+
+            expect(renderedImages.length).toBe(2);
+
+            let index = 0;
+            renderedImages.forEach((child: any) => {
+                const imageUrl = child.prop("src");
+
+                expect(imageUrl).toBe(images[index++].url);
+            });
+        });
     });
 });
--- src/com/mendix/widget/carousel/components/__tests__/CarouselItem.spec.ts
+++ src/com/mendix/widget/carousel/components/__tests__/CarouselItem.spec.ts
@@ -1,23 +1,33 @@
 import { ShallowWrapper, shallow } from "enzyme";
-import { DOM, createElement } from "react";
+import { createElement } from "react";
 
 import { CarouselItem, CarouselItemProps } from "../CarouselItem";
 
 describe("CarouselItem", () => {
     const url = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
-    let wrapper: ShallowWrapper<CarouselItemProps, any>;
-    beforeEach(() => wrapper = shallow(createElement(CarouselItem, { url, active: true })));
+    let output: ShallowWrapper<CarouselItemProps, any>;
 
-    it("should be active", () => {
-        expect(wrapper).toBeElement(
-            DOM.div({ className: "item active" }, DOM.img({ alt: "Carousel image", src: url }))
-        );
+    beforeEach(() => output = shallow(createElement(CarouselItem, { url, active: true })));
+
+    it("should be a div with one img child", () => {
+        expect(output.type()).toBe("div");
+        expect(output.children().length).toBe(1);
+        expect(output.children().first().type()).toBe("img");
     });
 
-    it("should be inactive", () => {
-        wrapper = shallow(createElement(CarouselItem, { url, active: false }));
-        expect(wrapper).toBeElement(
-            DOM.div({ className: "item" }, DOM.img({ alt: "Carousel image", src: url }))
-        );
+    it("should be active as set in the props", () => {
+        expect(output.hasClass("active")).toBe(true);
+    });
+
+    it("should be inactive as set in the props", () => {
+        output = shallow(createElement(CarouselItem, { url, active: false }));
+
+        expect(output.hasClass("active")).toBe(false);
+    });
+
+    it("should have an image with the specified url", () => {
+        let imageUrl = output.children().first().prop("src");
+
+        expect(imageUrl).toBe(url);
     });
 });
