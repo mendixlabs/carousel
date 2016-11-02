41a2670e16c1ae9e870807bec656406fbbff0862
Edwin P. Magezi <emagezi@gmail.com>
2016-11-02 13:03:19 +0300

    Add Control component tests outline

---
+++ src/com/mendix/widget/carousel/components/__tests__/Control.spec.ts
@@ -0,0 +1,22 @@
+describe("Control", () => {
+
+    xit("renders the structure correctly", () => {
+        //
+    });
+
+    xit("renders with class carousel-control", () => {
+        //
+    });
+
+    xit("renders direction css class", () => {
+        //
+    });
+
+    xit("renders direction icon", () => {
+        //
+    });
+
+    xit("responds to single click event", () => {
+        //
+    });
+});
