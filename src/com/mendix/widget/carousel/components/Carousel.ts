d2967bbf7c96b6bd418408b51be821479367e659
Edwin P. Magezi <emagezi@gmail.com>
2016-12-15 23:26:54 +0300

    Change from to transform3D based navigation

--- src/com/mendix/widget/carousel/components/Carousel.ts
+++ src/com/mendix/widget/carousel/components/Carousel.ts
@@ -24,12 +24,18 @@ interface CarouselState {
 
 export class Carousel extends Component<CarouselProps, CarouselState> {
     static defaultProps: CarouselProps = { images: [] };
+    private carouselItemsNode: HTMLElement;
+    private carouselNode: HTMLElement;
     private moveToTheLeft: MouseEventHandler<HTMLDivElement>;
     private moveToTheRight: MouseEventHandler<HTMLDivElement>;
+    private carouselWidth: number;
+    private items: HTMLElement[];
 
     constructor(props: CarouselProps) {
         super(props);
 
+        this.items = [];
+        this.carouselWidth = 0;
         this.moveToTheLeft = () => this.moveInDirection("left");
         this.moveToTheRight = () => this.moveInDirection("right");
         this.state = { activeIndex: 0 };
@@ -38,8 +44,12 @@ export class Carousel extends Component<CarouselProps, CarouselState> {
     render() {
         return DOM.div({ className: "widget-carousel-wrapper" },
             createElement(Alert, { message: this.state.alertMessage }),
-            DOM.div({ className: "widget-carousel" },
-                DOM.div({ className: "widget-carousel-item-wrapper" },
+            DOM.div({ className: "widget-carousel", ref: (node: HTMLElement) => { this.carouselNode = node; } },
+                DOM.div(
+                    {
+                        className: "widget-carousel-item-wrapper",
+                        ref: (node: HTMLElement) => { this.carouselItemsNode = node; }
+                    },
                     this.createCarouselItems(this.props.images, this.state.activeIndex || 0)
                 ),
                 this.createCarouselControls()
@@ -47,10 +57,28 @@ export class Carousel extends Component<CarouselProps, CarouselState> {
         );
     }
 
+    componentDidMount() {
+        this.items.map((carouselItem: HTMLElement) => {
+            if (carouselItem) {
+                if (!this.carouselWidth) {
+                    this.carouselWidth = carouselItem.parentElement ? carouselItem.parentElement.offsetWidth : 0;
+                    carouselItem.style.width = this.carouselWidth + "px";
+                } else {
+                    carouselItem.style.width = this.carouselWidth + "px";
+                }
+            }
+        });
+        this.carouselItemsNode.style.width = (this.items.length * this.carouselWidth) + "px";
+        setTimeout(() => {
+            this.carouselNode.style.height = this.items[0].offsetHeight + "px";
+        }, 100);
+    }
+
     private createCarouselItems(images: Image[], activeIndex: number) {
         return images.map((image, index) =>
             createElement(CarouselItem, {
                 active: index === activeIndex,
+                getRef: (ref: HTMLElement) => this.setCarouselItemPosition(ref),
                 key: index,
                 onClick: () => this.executeAction(image.onClickMicroflow, image.onClickForm),
                 url: image.url
@@ -74,12 +102,20 @@ export class Carousel extends Component<CarouselProps, CarouselState> {
         ];
     }
 
+    private setCarouselItemPosition(carouselItem: HTMLElement) {
+        this.items.push(carouselItem);
+    }
+
     private moveInDirection(direction: "right" | "left") {
         const { activeIndex } = this.state;
         const firstIndex = 0;
         const newActiveIndex = direction === "right"
-            ? activeIndex < this.props.images.length - 1 ? activeIndex + 1 : firstIndex
+            ? activeIndex < this.props.images.length - 1 ? activeIndex + 1 : -firstIndex
             : activeIndex === firstIndex ? this.props.images.length - 1 : activeIndex - 1;
+
+        const carouselItem = this.items[newActiveIndex];
+        this.carouselItemsNode.style.transform = `translate3d(${-(newActiveIndex * this.carouselWidth)}px, 0px, 0px)`;
+        this.carouselNode.style.height = carouselItem.offsetHeight + "px";
 
         this.setState({ activeIndex: newActiveIndex, alertMessage: "" });
     }
--- src/com/mendix/widget/carousel/components/CarouselItem.ts
+++ src/com/mendix/widget/carousel/components/CarouselItem.ts
@@ -5,9 +5,15 @@ export interface CarouselItemProps {
     url: string;
     active: boolean;
     onClick?: (event: MouseEvent<HTMLDivElement>) => void;
+    getRef?: (ref: HTMLElement) => void;
 }
 
 export const CarouselItem: StatelessComponent<CarouselItemProps> = (props) =>
-    DOM.div({ className: classNames("widget-carousel-item", { active: props.active }), onClick: props.onClick },
+    DOM.div(
+        {
+            className: classNames("widget-carousel-item", { active: props.active }),
+            onClick: props.onClick,
+            ref: (node: HTMLElement) => { if (props.getRef) { props.getRef(node); } }
+        },
         DOM.img({ alt: "Carousel image", src: props.url })
     );
