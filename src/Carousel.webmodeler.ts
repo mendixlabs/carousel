0b12d927978efe2c98d4d7e487d6b19d19065d31
Edwin P. Magezi <emagezi@gmail.com>
2017-06-02 12:59:45 +0300

    Update preview css implementation

--- src/Carousel.webmodeler.ts
+++ src/Carousel.webmodeler.ts
@@ -4,7 +4,6 @@ import { Carousel, Image } from "./components/Carousel";
 import CarouselContainer, { CarouselContainerProps } from "./components/CarouselContainer";
 import { Alert } from "./components/Alert";
 
-import * as css from "./ui/Carousel.scss";
 // tslint:disable-next-line
 const image = require("base64-image-loader!./img/Preview.jpg");
 
@@ -12,10 +11,6 @@ declare function require(url: string): string;
 
 // tslint:disable class-name
 export class preview extends Component<CarouselContainerProps, {}> {
-    componentWillMount() {
-        this.addPreviewStyle("widget-carousel");
-    }
-
     render() {
         const validationAlert = CarouselContainer.validateProps(this.props);
         if (validationAlert) {
@@ -35,18 +30,8 @@ export class preview extends Component<CarouselContainerProps, {}> {
 
         return [ { url: image } ];
     }
+}
 
-    private addPreviewStyle(styleId: string) {
-        // This workaround is to load style in the preview temporary till mendix has a better solution
-        const iFrame = document.getElementsByClassName("t-page-editor-iframe")[0] as HTMLIFrameElement;
-        const iFrameDoc = iFrame.contentDocument;
-        if (!iFrameDoc.getElementById(styleId)) {
-            const styleTarget = iFrameDoc.head || iFrameDoc.getElementsByTagName("head")[0];
-            const styleElement = document.createElement("style");
-            styleElement.setAttribute("type", "text/css");
-            styleElement.setAttribute("id", styleId);
-            styleElement.appendChild(document.createTextNode(css));
-            styleTarget.appendChild(styleElement);
-        }
-    }
+export function getPreviewCss() {
+    return require("./ui/Carousel.scss");
 }
